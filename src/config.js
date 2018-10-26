62d7a932f278620de300c2820be9e57234dd668e
C790266922 <790266922@qq.com>
2018-10-27 00:26:20 +0800

    update

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+	chain: 'http://121.196.200.225:1337',
+  	privateKey: '1f3ed395f6ec32467d905b62f6b3eed5100db80e7d6fa4f9390b5e0cabc06671',
+  	contractAddress: '0x45bf81c159f7f531b9d27d4035f66fa7670f4b29'
+}
+module.exports = config
